1fadbe470e44374246dc08c9a39a7705a217b996
Manasa <manasa.rupireddy@gmail.com>
2017-04-11 15:09:25 -0700

    First commit

---
+++ src/JavaBasics.java
@@ -0,0 +1,13 @@
+
+public final class JavaBasics {
+	final String pancardNumber;
+
+	public JavaBasics(String pancardNumber) {
+		this.pancardNumber = pancardNumber;
+	}
+
+	public String getPancardNumber() {
+		return pancardNumber;
+	}
+
+}
---
+++ src/Test.java
@@ -0,0 +1,8 @@
+
+public class Test {
+
+	public static void main(String[] args) {
+		JavaBasics obj = new JavaBasics("100");
+		System.out.println(obj.getPancardNumber());
+	}
+}
